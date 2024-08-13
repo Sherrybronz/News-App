aa8b457d3e6928ed3ce54802a5ca6a0d486a2b8c
Sherrybronz <135938785+Sherrybronz@users.noreply.github.com>
2024-08-14 00:21:50 +0530

    Update NewsApp.py

--- NewsApp.py
+++ NewsApp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 def fetch_news(api_key, topic):
-    url = f'https://newsapi.org/v2/everything?q={topic}&sortBy=country&apiKey={api_key}'
+    url = f'https://newsapi.org/v2/everything?q={topic}&sortBy=country=in&apiKey={api_key}'
     response = requests.get(url)
     if response.status_code == 200:
         news_data = response.json()
