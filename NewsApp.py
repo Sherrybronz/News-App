3e5ddb87ac4da106137ed084f3452894c891a3bd
Sherrybronz <135938785+Sherrybronz@users.noreply.github.com>
2024-08-21 12:59:15 +0530

    Update NewsApp.py

--- NewsApp.py
+++ NewsApp.py
@@ -32,7 +32,7 @@ st.write("Select a topic to read news articles:")
 st.write("""
     <style>
     body {
-        background-image: url("Naruto.jpg");
+        image: url("Naruto.jpg");
         background-size: cover;
     }
     </style>
